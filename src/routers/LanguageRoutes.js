72835f7b085deb2eb1dd4b9444e07992c34e4295
TrNgTien <ngoctien27122001@gmail.com>
2022-11-29 12:04:28 +0700

    Tien | Adjust | Fix naming

--- src/routers/LanguageRoutes.js
+++ src/routers/LanguageRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const LanguageController = require("../controller/LanguageController.js");
+const LanguageController = require("../controllers/LanguageController.js");
 
 router.post("/check", LanguageController.checkValidLanguage);
 
