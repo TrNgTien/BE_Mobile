782d214bf0eb3c846009d1bb7b8c3bb1ca37f52b
Tien Tran <66483590+TrNgTien@users.noreply.github.com>
2022-11-29 11:58:42 +0700

    Tien | Add | Finish feature checking data (#1)

--- src/services/LanguageService.js
+++ src/services/LanguageService.js
@@ -1,5 +1,15 @@
+const dictionary = require("@vntk/dictionary");
+
 module.exports = {
   checkValidLanguage: async (req, res) => {
-    return res.send("REST called successfully!");
+    const clientText = req.body.text;
+    if (dictionary.has(clientText)) {
+      return res.status(200).json({
+        message: "Hợp lệ",
+      });
+    }
+    return res.status(400).json({
+      message: "Không Hợp lệ",
+    });
   },
 };
